46a117fa076f5ca7640f552b82921710a1024ea2
dj5 <dj5>
2019-03-26 17:55:01 +0530

    commit-1

---
+++ app/src/main/java/example/com/vishwamedi/CasePostFragment.java
@@ -0,0 +1,33 @@
+package example.com.vishwamedi;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CasePostFragment extends Fragment {
+
+
+    public CasePostFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v= inflater.inflate(R.layout.fragment_case_post, container, false);
+
+
+        return v;
+    }
+
+
+}
