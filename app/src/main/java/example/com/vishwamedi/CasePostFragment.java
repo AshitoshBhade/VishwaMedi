ac2356d04b185ce4730816f4ba66ffae7d41a129
dj5 <dj5>
2019-03-27 00:57:10 +0530

    commit-2

--- app/src/main/java/example/com/vishwamedi/CasePostFragment.java
+++ app/src/main/java/example/com/vishwamedi/CasePostFragment.java
@@ -1,17 +1,37 @@
 package example.com.vishwamedi;
 
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CasePostFragment extends Fragment {
+
+
+    private Button postCaseBtn,caseDateBtn,admissionDateBtn,submitBtn;
+
+    private EditText trackId,compName,patientName,hospitalName,phoneNo,
+            treatingDoc,addr,landmark,state,dist,pin,vendor,remark,verifier;
+
+    private CasePostFragment casePostFragment;
+
+
+    String caseDateStr,dateOfAdmissionStr;
+    int mYear,mMonth,mDay;
 
 
     public CasePostFragment() {
@@ -26,8 +46,135 @@ public class CasePostFragment extends Fragment {
         View v= inflater.inflate(R.layout.fragment_case_post, container, false);
 
 
+        trackId=v.findViewById(R.id.TrackId);
+        compName=v.findViewById(R.id.CompanyName);
+        patientName=v.findViewById(R.id.PatientName);
+        hospitalName=v.findViewById(R.id.HospitalName);
+        phoneNo=v.findViewById(R.id.Phone);
+        treatingDoc=v.findViewById(R.id.TreatingDoctor);
+        addr=v.findViewById(R.id.Address);
+        landmark=v.findViewById(R.id.LandMark);
+        state=v.findViewById(R.id.State);
+        dist=v.findViewById(R.id.District);
+        pin=v.findViewById(R.id.Pincode);
+        vendor=v.findViewById(R.id.Vendor);
+        remark=v.findViewById(R.id.Remarks);
+        verifier=v.findViewById(R.id.VerifierName);
+
+
+        caseDateBtn=v.findViewById(R.id.CaseDateButton);
+        admissionDateBtn=v.findViewById(R.id.DateOfAdmissionButton);
+        submitBtn=v.findViewById(R.id.SubmitButton);
+
+
+        caseDateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(),
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+                                mMonth=month+1;
+
+                                month=month+1;
+
+                                if(month<10) {
+                                    caseDateStr = year + "-0" + month + "-" + dayOfMonth;
+                                }
+                                else
+                                {
+                                    caseDateStr = year + "-0" + month + "-" + dayOfMonth;
+                                }
+
+                                if (!caseDateStr.isEmpty())
+                                {
+                                    try {
+                                        Date date =new SimpleDateFormat("dd/MM/yyyy").parse(caseDateStr);
+                                    } catch (ParseException e) {
+                                        e.printStackTrace();
+                                    }
+                                    caseDateBtn.setText(caseDateStr);
+
+                                }else
+                                {
+                                    caseDateBtn.setError("Could Not Found Date");
+                                }
+
+
+                            }
+
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            }
+
+        });
+
+
+        admissionDateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(),
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+                                mMonth=month+1;
+
+                                month=month+1;
+
+                                if(month<10) {
+                                    dateOfAdmissionStr = year + "-0" + month + "-" + dayOfMonth;
+                                }
+                                else
+                                {
+                                    dateOfAdmissionStr = year + "-0" + month + "-" + dayOfMonth;
+                                }
+
+                                if (!dateOfAdmissionStr.isEmpty())
+                                {
+                                    try {
+                                        Date date =new SimpleDateFormat("dd/MM/yyyy").parse(dateOfAdmissionStr);
+                                    } catch (ParseException e) {
+                                        e.printStackTrace();
+                                    }
+                                    admissionDateBtn.setText(caseDateStr);
+
+                                }else
+                                {
+                                    admissionDateBtn.setError("Could Not Found Date");
+                                }
+
+
+                            }
+
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+
+            }
+        });
+
+
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
+
         return v;
     }
+
+
 
 
 }
--- app/src/main/java/example/com/vishwamedi/MainActivity.java
+++ app/src/main/java/example/com/vishwamedi/MainActivity.java
@@ -1,13 +1,94 @@
 package example.com.vishwamedi;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private CasePostFragment casePostFragment;
+    private BottomNavigationView navigationView;
+    private android.support.v7.widget.Toolbar mtoolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        navigationView = findViewById(R.id.bottom_navigation);
+        mtoolbar = findViewById(R.id.MainToolbar);
+
+        setSupportActionBar(mtoolbar);
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+                switch (item.getItemId()) {
+                    case R.id.HomeNav:
+                        return true;
+
+                    case R.id.PostCaseNav:
+                        casePostFragment = new CasePostFragment();
+                        setFragment(casePostFragment);
+                        return true;
+
+                    case R.id.AccountNav:
+                        return true;
+
+                    default: return false;
+
+                }
+
+
+            }
+        });
+
+
+    }
+
+    private void setFragment(Fragment fragment) {
+
+        android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction()
+                .add(fragment,"PostCaseFragment").addToBackStack("PostCaseFragment");
+        fragmentTransaction.replace(R.id.MainLayout, fragment);
+        fragmentTransaction.commit();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+       super.onCreateOptionsMenu(menu);
+
+       getMenuInflater().inflate(R.menu.main_menu,menu);
+       return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+      super.onOptionsItemSelected(item);
+
+      switch (item.getItemId())
+      {
+          case R.id.logoutMenu:
+
+              //SignOut();
+                return true;
+
+          default: return false;
+      }
     }
 }
